c456a3278d85213295078117cacdf0844b87e139
lironh <lironenk@gmail.com>
2022-03-20 00:27:06 +0200

    feat(exec-command): dummy run resolved wip

--- src/command_exec_utils.rs
+++ src/command_exec_utils.rs
@@ -1,33 +1,37 @@
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
-use std::process::Command;
-use std::{error::Error};
+use std::error::Error;
+use std::process::{Command, Stdio};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Script {
     pub(crate) script: String,
 }
 
-pub fn exec_scripts(command: &str, projects: Vec<String>) -> Result<(), Box<dyn Error>> {
-   // let result: Map<String, Value> = Map::new();
-   let output = Command::new("npm").arg("run").arg(command).output()?;
+pub fn exec_scripts(
+    command: &str,
+    projects: Vec<&str>,
+) -> Result<Map<String, Value>, Box<dyn Error>> {
+    let result: Map<String, Value> = Map::new();
 
-    println!("{:?}", projects);
-    if !output.status.success() {
-        println!("Command executed with failing error code");
+    for p in projects {
+        let mut cmd = Command::new("npm");
+        cmd.current_dir(p);
+        cmd.arg("run").arg("dev").spawn();
+            // .stdout(Stdio::piped()).spawn();
+        let output = cmd.output()?;
+
+        if !output.status.success() {
+            println!("Command executed with failing error code");
+        }
+
+        String::from_utf8(output.stdout)?
+            .lines()
+            // .take(5)
+            .for_each(|x| println!("{:?}", x));
+
+        // println!("project {:?}", p);
     }
 
-    String::from_utf8(output.stdout)?
-        .lines()
-        .take(5)
-        .for_each(|x| println!("{:?}", x));
-
-
-    // for p in projects {
-    //     let output = Command::new("npm run command").arg("log").arg("--oneline").output()?;
-    //
-    //     println!("project {:?}", p);
-    // }
-
-    Ok(())
+    Ok(result)
 }
