a9b8c002691410531d90f3262f3da8573c31a324
lironh <lironenk@gmail.com>
2022-04-02 11:27:31 +0300

    feat(webapp): better UX

--- webapp/pages/runner-stepper/step-two-content.tsx
+++ webapp/pages/runner-stepper/step-two-content.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react';
+import { useState } from 'react';
 import StepContent from '@mui/material/StepContent';
-import Button from '@mui/material/Button';
 import {List, ListItem, ListItemText, TextField} from "@mui/material";
-import styles from '../../styles/Stepper.module.css'
 import {useEffect} from "react";
 import {getCommands} from "../api/dev-runner-api";
 
 
 export default function StepTwoContent() {
 
-    const [commands, setCommands] = React.useState(['']);
+    const [commands, setCommands] = useState(['']);
 
     useEffect(() => {
         getCommands().then((res: {scripts: string[]}) => {
